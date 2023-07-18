3c2af798823c90466be05adf650452f65393d24a
Ekaterina Gapizova <Kat1502@mail.ru>
2023-07-18 16:00:05 +0500

    Добавление верификатора Яндекс 3

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -17,6 +17,10 @@ module.exports = {
             template: path.resolve(__dirname, './src/index.html'), // шаблон
             filename: 'index.html', // название выходного файла
         }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/yandex_d790d6507a3c5cd6.html'), // шаблон
+            filename: 'yandex_d790d6507a3c5cd6.html', // название выходного файла
+        }),
         new FaviconsWebpackPlugin(),
     ],
     module: {
